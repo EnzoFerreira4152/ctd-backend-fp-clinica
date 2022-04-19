27cb4dfec12840c147ce02150140d12a1e83469d
EnzoFerreira4152 <enzo_al_17@hotmail.com>
2022-04-19 03:06:26 -0300

    Eliminé el service de domicilio / Agregué anotacion service de spring al service de turnos

--- src/main/java/com/backend/finalProject/service/impl/AppointmentService.java
+++ src/main/java/com/backend/finalProject/service/impl/AppointmentService.java
@@ -5,9 +5,11 @@ import com.backend.finalProject.persistence.repository.IAppointmentRepository;
 import com.backend.finalProject.service.IAppointmentService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class AppointmentService implements IAppointmentService {
 
     private final IAppointmentRepository repository;
